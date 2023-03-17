4808f43a5b5361abc91b70e74ddb9f0832585077
Anusorn Kongthong <>
2023-03-17 11:16:49 +0700

    a

--- lib/pages/stock_edit_detail_view/stock_edit_detail_view_widget.dart
+++ lib/pages/stock_edit_detail_view/stock_edit_detail_view_widget.dart
@@ -7,6 +7,7 @@ import '/flutter_flow/flutter_flow_widgets.dart';
 import '/custom_code/actions/index.dart' as actions;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import '/flutter_flow/custom_functions.dart' as functions;
@@ -36,6 +37,15 @@ class _StockEditDetailViewWidgetState extends State<StockEditDetailViewWidget> {
   void initState() {
     super.initState();
     _model = createModel(context, () => StockEditDetailViewModel());
+
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      if (widget.stockParameter == null) {
+        context.goNamed('StockListPage');
+
+        return;
+      }
+    });
 
     _model.trackingCodeController ??= TextEditingController(text: widget.stockParameter!.trackingCode);
     _model.roomNoController ??= TextEditingController(text: widget.stockParameter!.roomNo);
